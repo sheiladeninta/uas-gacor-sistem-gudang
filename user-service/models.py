fd1cfc4c39dfe63f097851ef9d33572a7f4e8a25
Fajarjauza <fajarjauza9@gmail.com>
2025-06-13 02:27:21 +0700

    done

--- user-service/models.py
+++ user-service/models.py
@@ -1,6 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
+from enum import Enum
 
 db = SQLAlchemy()
+
+class UserRole(Enum):
+    STAFF = 'staff'
+    CLIENT = 'client'
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -8,6 +13,10 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nama = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String(100), nullable=False)
+    role = db.Column(db.String(10), nullable=False, default=UserRole.CLIENT.value)
+    email = db.Column(db.String(120), unique=True, nullable=True)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), onupdate=db.func.now())
 
     def __repr__(self):
-        return f'<User {self.nama}>'
+        return f'<User {self.nama} ({self.role})>'
